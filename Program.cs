5a510e23d022e64f68bb2f7813ef02b52f4b235f
Noe <VaescoNoe@gmail.com>
2019-04-23 00:02:14 -0500

    Hola Mundo C Sharp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("HOLA MUNDO");
+        string i = Console.ReadLine();
+        Console.WriteLine(i);
+        Console.ReadLine();
+    }
+}
